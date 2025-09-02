25afc920fcbfa5fdb8f88b3ac1a60e71618d5c44
dpalexandru <alexpoian91@hotmail.com>
2025-09-02 16:11:13 +0200

    add: two separated lists, one for actors and one for actress

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,9 @@ function App() {
   const urlOne = "https://lanciweb.github.io/demo/api/actresses/"
   const urlTwo = "https://lanciweb.github.io/demo/api/actors/"
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
-  //chiamata axios per recuperare data
-  const fatchData = () =>
-    axios.get(urlOne).then((resp) => {
-      setActresses(resp.data)
-      console.log(actresses);
-    })
+
 
   //carico dati all' caricamento della pagina 
   useEffect(() => {
@@ -21,7 +17,11 @@ function App() {
         setActresses(resp.data);
         console.log(resp.data);
       })
-
+    axios.get(urlTwo)
+      .then((resp) => {
+        setActors(resp.data);
+        console.log(resp.data);
+      })
   }, []);
 
   return (
@@ -31,10 +31,33 @@ function App() {
 
       </header>
       <div className="container">
+        <h2 className="text-center mt-3">Actress:</h2>
+
         <div className="row">
 
           <div className="row g-3">
             {actresses.map((actress) => (
+              <div className="col-4" key={actress.id}>
+                <div className="card h-100 shadow-sm">
+                  <img src={actress.image} className="card-img-top" alt={actress.name} />
+                  <div className="card-body">
+                    <h5 className="card-title mb-2">{actress.name}</h5>
+                    <p className="card-text">{actress.birth_year}</p>
+                    <p className="card-text">{actress.nationality}</p>
+                    <p className="card-text">{actress.biography}</p>
+                    <p className="card-text">Awards: <br />{actress.awards}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <h2 className="text-center mt-3">actors:</h2>
+        <div className="row">
+
+          <div className="row g-3">
+            {actors.map((actress) => (
               <div className="col-4" key={actress.id}>
                 <div className="card h-100 shadow-sm">
                   <img src={actress.image} className="card-img-top" alt={actress.name} />
