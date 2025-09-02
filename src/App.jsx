1f92a4ca21299d44cf371e3f4404df0555b7357e
dpalexandru <alexpoian91@hotmail.com>
2025-09-02 15:26:06 +0200

    feat: show data from axios.get  in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const urlOne = "https://lanciweb.github.io/demo/api/actresses/"
+  const urlTwo = "https://lanciweb.github.io/demo/api/actors/"
+  const [actresses, setActresses] = useState([]);
+
+  //chiamata axios per recuperare data
+  const fatchData = () =>
+    axios.get(urlOne).then((resp) => {
+      setActresses(resp.data)
+      console.log(actresses);
+    })
+
+  //carico dati all' caricamento della pagina 
+  useEffect(() => {
+    axios.get(urlOne)
+      .then((resp) => {
+        setActresses(resp.data);
+        console.log(resp.data);
+      })
+
+  }, []);
 
   return (
     <>
       <h1>Hello World</h1>
+
     </>
   )
 }
